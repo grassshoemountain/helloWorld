3bcf70d5fa7ce560052b1d481200a6f9430dc552
Tianming Wu <tianming.wu.medphys@gmail.com>
2021-12-20 19:32:26 -0600

    initial commit

---
+++ helloWorld.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    vector<string> msg {"Hello", "c++", "World", "from", "vs code", "and the c++ extension!"};
+
+    for (const string& word : msg)
+    {
+        cout << word << " ";
+    }
+    cout << endl;
+}
